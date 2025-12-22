f3e9035ae99579f4c685ea4803689d49cd922bec
tasirin <167167911+tasirin1@users.noreply.github.com>
2025-12-22 15:39:56 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const settings = {
     "minecraft_version": "auto", // or specific version like "1.21.6"
-    "host": "127.0.0.1", // or "localhost", "your.ip.address.here"
-    "port": 55916, // set to -1 to automatically scan for open ports
-    "auth": "offline", // or "microsoft"
+    "host": "mishcreative.aternos.me", // or "localhost", "your.ip.address.here"
+    "port": 58495, // set to -1 to automatically scan for open ports
+    "auth": "online", // or "microsoft"
 
     // the mindserver manages all agents and hosts the UI
     "mindserver_port": 8080,
