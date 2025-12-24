b393b7903b7e0588681e95ecfea0f293f4614e5d
tasirin <167167911+tasirin1@users.noreply.github.com>
2025-12-24 14:51:01 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,9 +26,9 @@ const settings = {
         // individual profiles override values from the base profile
     ],
 
-    "load_memory": true, // load memory from previous session
-    "init_message": "Respond with hello world and your name", // sends to all on spawn
-"only_chat_with": [], // users that the bots listen to and send general messages to. if empty it will chat publicly
+    "load_memory": false, // load memory from previous session
+    "init_message": "Aku udah spawn",
+    "only_chat_with": [], // users that the bots listen to and send general messages to. if empty it will chat publicly
 
     "speak": false,
     // allows all bots to speak through text-to-speech. 
@@ -37,18 +37,18 @@ const settings = {
     // Works on windows and mac, but linux requires you to install the espeak package through your package manager eg: `apt install espeak` `pacman -S espeak`.
 
     "chat_ingame": true, // bot responses are shown in minecraft chat
-    "language": "en", // translate to/from this language. Supports these language names: https://cloud.google.com/translate/docs/languages
+    "language": "id", // translate to/from this language. Supports these language names: https://cloud.google.com/translate/docs/languages
     "render_bot_view": false, // show bot's view in browser at localhost:3000, 3001...
 
-    "allow_insecure_coding": true, // allows newAction command and model can write/run code on your computer. enable at own risk
+    "allow_insecure_coding": false, // allows newAction command and model can write/run code on your computer. enable at own risk
     "allow_vision": false, // allows vision model to interpret screenshots as inputs
     "blocked_actions" : ["!checkBlueprint", "!checkBlueprintLevel", "!getBlueprint", "!getBlueprintLevel"] , // commands to disable and remove from docs. Ex: ["!setMode"]
     "code_timeout_mins": -1, // minutes code is allowed to run. -1 for no timeout
     "relevant_docs_count": 5, // number of relevant code function docs to select for prompting. -1 for all
 
-    "max_messages": 15, // max number of messages to keep in context
-    "num_examples": 2, // number of examples to give to the model
-    "max_commands": -1, // max number of commands that can be used in consecutive responses. -1 for no limit
+    "max_messages": 10, // max number of messages to keep in context
+    "num_examples": 1, // number of examples to give to the model
+    "max_commands": 3, // max number of commands that can be used in consecutive responses. -1 for no limit
     "show_command_syntax": "full", // "full", "shortened", or "none"
     "narrate_behavior": true, // chat simple automatic actions ('Picking up item!')
     "chat_bot_messages": true, // publicly chat messages to other bots
